2505b5ce23537fd338e6b2b82c131f5e27705536
AsmaaElgharib <asmaa.samir.elgharib@gmail.com>
2025-12-25 00:29:14 +0200

    apply api requests

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./layout/header/header"; 
+import { Header } from "./layout/header/header";
+import { Shop } from "./features/shop/shop"; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Shop],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
+    stripePublicKey: 'pk_test_51RtskkGN7h7XSQM9iDo3QEYweN6Up3oFjC45hGow6vunegoj7CODFexkXunwdn22sXKJY0sS3Y0G5zGBPld7naPV003gTGnUSU'
+};
